af3013f22e92ac95404b06795ec3aacf98fd316f
Boscan-Gabriel <boscan1098@gmail.com>
2024-01-02 22:04:59 +0200

    LogIn and SignUp

--- checkmate-tw/src/App.js
+++ checkmate-tw/src/App.js
@@ -1,29 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import LogIn from "./Components/LogIn/LogIn";
+import SignUp from "./Components/SignUp/SignUp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          CheckMate
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<LogIn/>} /> 
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/" element={<LogIn/>} />
+      </Routes>
+    </Router>
   );
 }
 
