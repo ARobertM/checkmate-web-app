3a9bdf5963b2cb9bb1b541b25d36f4814e207d80
Sabin Arsene <sabinarsene56@gmail.com>
2024-01-12 00:26:27 +0200

    butonul bun asta e

--- frontend/checkmate-tw/src/Components/Home/Home.js
+++ frontend/checkmate-tw/src/Components/Home/Home.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 
 const Home = () => {
- 
   const handleAddEvent = () => {
     console.log("Deschide formularul pentru adăugarea unui nou eveniment.");
-   
 
+  };
+
+ 
   return (
     <div className="d-flex justify-content-center">
       <h2>Home sweet home Alabama</h2>
-      <button onClick={handleAddEvent} className="btn event">
+      <button onClick={handleAddEvent} className="btn btn-primary">
         Adaugă Eveniment
       </button>
     </div>
   );
 };
-}
+
 export default Home;
