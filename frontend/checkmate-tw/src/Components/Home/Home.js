fce8b6df30cc67108ad1b2a79f30943b68acfd6b
Sabin Arsene <sabinarsene56@gmail.com>
2024-01-12 00:19:32 +0200

    am adaugat un buton

--- frontend/checkmate-tw/src/Components/Home/Home.js
+++ frontend/checkmate-tw/src/Components/Home/Home.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 
 const Home = () => {
+ 
+  const handleAddEvent = () => {
+    console.log("Deschide formularul pentru adăugarea unui nou eveniment.");
+   
+
   return (
     <div className="d-flex justify-content-center">
       <h2>Home sweet home Alabama</h2>
+      <button onClick={handleAddEvent} className="btn event">
+        Adaugă Eveniment
+      </button>
     </div>
   );
 };
+}
 export default Home;
