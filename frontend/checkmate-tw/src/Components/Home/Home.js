dc8ab4dc280df7c4546dc699737393cfe8e0c1aa
ALEXANDRU ROBI <armihai1664@gmail.com>
2024-01-14 15:47:40 +0200

    home page juma

--- frontend/checkmate-tw/src/Components/Home/Home.js
+++ frontend/checkmate-tw/src/Components/Home/Home.js
@@ -6,10 +6,28 @@ const Home = () => {
   return (
     <div className="container">
       
-
-         <img className="logo" src='/logo_checkmate.png'></img>
-
+      <div className="pricipal-container">
+        
+        <span className="title">
+          <img className="logo" src='/logo_checkmate.png'></img>
+          
+          <div className="title-description">
+            <span className="title-text"> Welcome to, </span>
+            <span className="title-checkmate">Checkmate</span>
+            <span className="loggin-as">User: </span>
+            {/* de adaugat state cu user-ul sa-i apara numele */}
+            <button className="btn-sign-in">Sign Out</button> 
+            <button className="btn-about-us">About Us</button>
+          </div>   
+          <hr className="linie"></hr>
+        </span>
+      </div>
       
+      <div className="container-evenimente">
+        <button className="green-button">+</button>
+
+
+      </div>
     </div>
   );
 };
